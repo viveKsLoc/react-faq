b89ffbf952e3681b52d465785007ed3fc72bea16
Vivek R <theunresolvable@gmail.com>
2019-12-27 16:20:53 +0530

    Feature faq

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import FAQ from "./FAQ";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  return (
+    <div className="App">
+      <FAQ>
+        <FAQ.QAItem>
+          <FAQ.Question answerId="q1">
+            {(isOpen, onToggle) => {
+              return (
+                <>
+                  {isOpen ? "Open " : "Close "}
+                  <span>What is your question?</span>
+                </>
+              );
+            }}
+          </FAQ.Question>
+          <FAQ.Answer id="q1"> Knock Knock! </FAQ.Answer>
+        </FAQ.QAItem>
+        <FAQ.QAItem>
+          <FAQ.Question answerId="q2">
+            {(isOpen, onToggle) => {
+              return (
+                <>
+                  {isOpen ? "Open " : "Close "}
+                  <span>Why do you ask such stupid questions?</span>
+                </>
+              );
+            }}
+          </FAQ.Question>
+          <FAQ.Answer id="q2"> 42 is the answer. </FAQ.Answer>
+        </FAQ.QAItem>
+        <FAQ.QAItem>
+          <FAQ.Question answerId="q3">
+            {(isOpen, onToggle) => {
+              return (
+                <>
+                  {isOpen ? "Open " : "Close "}
+                  <span>Any other questions?</span>
+                </>
+              );
+            }}
+          </FAQ.Question>
+          <FAQ.Answer id="q3"> Seek and you shall find. </FAQ.Answer>
+        </FAQ.QAItem>
+      </FAQ>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
